7c9bedfe356f08020ab7e01f83e56bf620f91563
100RAB <115261525+100rabx23@users.noreply.github.com>
2026-03-12 16:01:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+
+const showRegister = document.getElementById("showRegister");
+const showLogin = document.getElementById("showLogin");
+
+
+/* Toggle Forms */
+
+showRegister.onclick = () => {
+loginForm.classList.remove("active");
+registerForm.classList.add("active");
+}
+
+showLogin.onclick = () => {
+registerForm.classList.remove("active");
+loginForm.classList.add("active");
+}
+
+
+/* Redirect after Login */
+
+loginForm.addEventListener("submit", function(e){
+e.preventDefault();
+
+alert("Login Successful!");
+
+window.location.href = "YOUR_LINK_HERE";
+});
+
+
+/* Redirect after Register */
+
+registerForm.addEventListener("submit", function(e){
+e.preventDefault();
+
+alert("Registration Successful!");
+
+window.location.href = "YOUR_LINK_HERE";
+});
